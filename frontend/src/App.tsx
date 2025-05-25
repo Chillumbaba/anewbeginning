dd5d422c7ed5bcf5dd2630c36966b492c33f7672
Krishnan Padmanabhan <kp@ibm.com>
2025-05-25 22:46:26 +0100

    feat: add frontend public files and basic React components

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>A New Beginning</h1>
+        <p>Welcome to your text entry application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
